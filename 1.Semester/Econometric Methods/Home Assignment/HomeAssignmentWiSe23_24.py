7d3edd3368f0b5c2faa893665caafa747228b7d6
Robert Hennings <hennings.robert@web.de>
2024-02-11 15:59:17 +0100

    Final Version of Home Assignment

--- 1.Semester/Econometric Methods/Home Assignment/HomeAssignmentWiSe23_24.py
+++ 1.Semester/Econometric Methods/Home Assignment/HomeAssignmentWiSe23_24.py
@@ -1,11 +1,8 @@
 # Econometric Methods Home Assignment WiSe 23/24
 """
 Group Members:
-1. stu, stuXXXX
-2. stu, stuXXXX
-3. stu, stuXXXX
-4. stu, stuXXXX
-5. stu, stuXXXX
+Josef Fella
+Robert Hennings
 """
 import pandas as pd
 import numpy as np
@@ -103,15 +100,18 @@ lin_reg_fit.summary()
 # Remember this variable was binary
 lin_reg_fit.params["smkban"]
 # -0.04805196345001899
-# The estimated parameter of the smkban has the desired sign and
-# is about: -0.04805196345001899 what would mean that if a smoking ban exists at the work
-# place (smkban=1) it reduces the likelihood that the individual smokes since
-# the parameters reduces the result by its sign, its magnitude is rather small
-# being roughly -0.05 what could indicate the strength of the taken measures at
-# the work place
-# Since we are working here with a linear probability model, we can state that
-# beta_hat_1 can be interpreted as the change in the probability that the
-# dependent variable smoker is 1, holding all other factors constant
+# H0: measures at the work place banning smoking have no effect on becoming a smoker, βˆ1 is equal to 0
+# H1: measures indeed have an effect, βˆ1 != 0
+
+# - If we look purely on the p-value of 0.000 we would argue that its statistically
+# significant, since its below the significance level of 0.05 and even 0.01. So we
+# have statistical evidence to reject our H0 (less smoking with smoking ban).
+# - On the other hand the coefficient is -0.048052, so smoking measures in place
+# at the work area decrease the predicted probability of being a smoker by 4.8052 %.
+# In absolute terms this is a quiet small magnitude. Rendering the effect strength of the measures taken questionable.
+
+# --> Statistically relevant BUT practical importance will be dependent of the study and scale of variable involved.
+
 plt.plot(smoking_data.index, smoking_data.smoker, "o")
 plt.plot(smoking_data.index, lin_reg_fit.fittedvalues, "ro")
 plt.title("True values vs. Model fitted values", fontsize=12)
@@ -131,28 +131,35 @@ st.norm.pdf(z_score)
 abs(t_test_smkban.summary_frame()["t"])
 
 # c)
-# Does the probability of smoking increase or decre- ase with the level of education?
+# Does the probability of smoking increase or decrease with the level of education?
 # Observe the educational related variables: hsdrop, hsgrad, colsome, colgrad,
 lin_reg_fit.summary()
-# Observing the educational related variables inlcuded in the model one can see
+# Observing the educational related variables inlcuded in the model, one can see
 # the following trend: 
 # the higher the education of an individual the less likely or the smaller the chance
 # that this individual is a smoker, but all estimated parameters are positive
-# as high school drop outs have an effect of 0.2822, high school grads: 0.2246
-# attended a college: 0.1556 and finally college grads are least likely ending up
-# as a smoker with: 0.0433
-abs(lin_reg_fit.params.diff())
+# as high school drop outs have an effect of 0.2821909, high school grads: 0.2245546
+# attended a college: 0.1556154 and finally college grads are least likely ending up
+# as a smoker with: 0.0433244
 # where the effect or absolute difference is biggest between attended college and
 # the college grads, so finishing college is advised
 
-# Perform Wald test for joint parameter test
+# --> Model implies higher probability for hsfropouts than for graduates
+abs(lin_reg_fit.params.diff())
+
+# H0 Hypothesis: hsdrop and hsgrad have the same effect on smoker: hsdrop = hsgrad
 # test if hsdrop and hsgrad have the same effect for the smoking probability
+# Wald test
 hypothesis_0 = ' = '.join(["hsdrop", "hsgrad"])
 wald_test = lin_reg_fit.wald_test(hypothesis_0)
 wald_test.statistic
 # chi2 statistic: 10.2478391
 # p-value: 0.0013684480785456001
-# Compare with critical value for test decision
+# - H0: hsdrop = hsgrad (they are equal)
+# - H1: hsdrop, hsgrad have different effects
+
+# - Result: We reject the H0 --> F = 10.25 and p-value 0.0014 (less than 0.05 and even 0.01)
+# - So both variables are statistically significantly different from each other
 
 
 # d)
@@ -172,8 +179,13 @@ y_pred = lin_reg_fit.predict(x_pred)
 y_pred
 # -0.01326874
 
-# Estimate seems to be very low, so the bespoke individual seems to be very 
-# unlikely to be a smoker and contradictingly has a negative sign
+# - smoking measures in place at the work area decrease the predicted probability
+#   of being a smoker for this individual specified, by roughly 1.32 %
+
+# Problems:
+# - We only assume linear relationship of predictor and regressor
+# - Especially the positvie coefficient of education may suggest a more complex relationship --> not     	considered here
+# - Predicted probability isn't standardized -> Can yield values above 1 o below 0
 
 # Linear probability model is not appropriate in this case
 smoking_data.query("age >= 70")
@@ -222,61 +234,101 @@ comp_eff_df = pd.DataFrame(index=["Linear Regression", "Logit Model", "Probit Mo
                            data=[lin_reg_eff, logit_eff, probit_eff],
                            columns=["Average Partiial Effects (APE)"])
 print(comp_eff_df)
+# H0: The APE is not relevant, it should be 0
+# H1: The APE is relevant and has an effect, it shoud be !=0
+
+# Result: Since the p-value is 0.000 what is <0.05 and even 0.01, we reject the H0
+# 		that the APE is not relevant, i.e. being equal to 0
+
+# Interpretation: the estimated average change in the probability of being a smoker is reduced by 4.81%
 # b)
 # Probit Model to calculate the effect for groups specified
 # Estimate the model new for the groups or predict for the groups?
 
 # i) male, 40 years old, college graduate.
-exog_probit_i = smoking_data.query("female == 0 and age == 40 and colgrad == 1").copy().reset_index(drop=True)
-probit_mod_i = sm.Probit(endog=exog_probit_i["smoker"], exog=exog_probit_i[exog_probit_i.columns[1:]])
-probit_mod_i_fit = probit_mod_i.fit()
-probit_mod_i_fit.summary()
-# Average Partial Effect (APE)
-probit_mod_i_ape = probit_mod_i_fit.get_margeff(at="mean", method="dydx", atexog="smkban")
-# estimate: smkban: -0.1600
-probit_mod_i_ape.summary()
-# -0.0485
-
-
-# ii) female, 20 years old, high school dropout.
-exog_probit_ii = smoking_data.query("female == 1 and age == 20 and hsdrop == 1").copy().reset_index(drop=True)
-probit_mod_ii = sm.Probit(endog=exog_probit_ii["smoker"], exog=exog_probit_ii[exog_probit_ii.columns[1:]])
-probit_mod_ii_fit = probit_mod_ii.fit()
-# numpy.linalg.LinAlgError: Singular matrix
-probit_mod_ii_fit.summary()
-# Average Partial Effect (APE)
-probit_mod_ii_ape = probit_mod_ii_fit.get_margeff(at="mean", method="dydx", atexog="smkban")
-# estimate: smkban:
-probit_mod_ii_ape.summary()
-
-
-# Alternative Approach for b)
-# Use the already fitted Probit Model and just predict for the set variables
-# i) male, 40 years old, college graduate.
-smkban_i = 1
+# Set the testing variables
+constant = 1
+smkban_i = 0
 age_i = 40
-age_sqrt_i = np.power(age_i, 2)
-hsdrop_i, hsgrad_i, colsome_i = 0, 0, 0
+age_sqrt_i = age_i**2
+hsdrop_i = 0
+hsgrad_i = 0
+colsome_i = 0
 colgrad_i = 1
 female_i = 0
-# Keep the correct order
-smoking_data[smoking_data.columns[1:]]
 
-x_i = [1, smkban_i, age_i, age_sqrt_i, hsdrop_i, hsgrad_i, colsome_i, colgrad_i, female_i]
+# Baseline Situation with no smoking ban in place
+probit_mod_fit.get_prediction(exog=[constant, smkban_i, age_i, age_sqrt_i, hsdrop_i, hsgrad_i, colsome_i, colgrad_i, female_i]).summary_frame()
+base_i = probit_mod_fit.get_prediction(exog=[constant, smkban_i, age_i, age_sqrt_i, hsdrop_i, hsgrad_i, colsome_i, colgrad_i, female_i]).predicted
+# 0.18671
 
-probit_mod_fit.predict(x_i)
-# 0.14684307
+# Situation when a smoking ban is introduced
+probit_mod_fit.get_prediction(exog=[constant, 1, age_i, age_sqrt_i, hsdrop_i, hsgrad_i, colsome_i, colgrad_i, female_i]).summary_frame()
+ban_i = probit_mod_fit.get_prediction(exog=[constant, 1, age_i, age_sqrt_i, hsdrop_i, hsgrad_i, colsome_i, colgrad_i, female_i]).predicted
+# 0.146843
+
+# Effect strenth as difference
+base_i - ban_i
+# 0.03986644
 
 
 # ii) female, 20 years old, high school dropout.
-smkban_ii = 1
+constant = 1
+smkban_ii = 0
 age_ii = 20
-age_sqrt_ii = np.power(age_ii, 2)
-hsdrop_ii, hsgrad_ii, colsome_ii = 1, 0, 0
+age_sqrt_ii = age_ii**2
+hsdrop_ii = 1
+hsgrad_ii = 0
+colsome_ii = 0
 colgrad_ii = 0
-female_ii = 0
+female_ii = 1
 
-x_ii = [1, smkban_ii, age_ii, age_sqrt_ii, hsdrop_ii, hsgrad_ii, colsome_ii, colgrad_ii, female_ii]
+# Baseline Situation with no smoking ban in place
+probit_mod_fit.get_prediction(exog=[constant, smkban_ii, age_ii, age_sqrt_ii, hsdrop_ii, hsgrad_ii, colsome_ii, colgrad_ii, female_ii]).summary_frame()
+base_ii = probit_mod_fit.get_prediction(exog=[constant, smkban_ii, age_ii, age_sqrt_ii, hsdrop_ii, hsgrad_ii, colsome_ii, colgrad_ii, female_ii]).predicted
+# 0.361454
 
-probit_mod_fit.predict(x_ii)
-# 0.34125012
+# Situation when a smoking ban is introduced
+probit_mod_fit.get_prediction(exog=[constant, 1, age_ii, age_sqrt_ii, hsdrop_ii, hsgrad_ii, colsome_ii, colgrad_ii, female_ii]).summary_frame()
+ban_ii = probit_mod_fit.get_prediction(exog=[constant, 1, age_ii, age_sqrt_ii, hsdrop_ii, hsgrad_ii, colsome_ii, colgrad_ii, female_ii]).predicted
+# 0.303431
+
+# Effect strenth as difference
+base_ii - ban_ii
+# 0.05802279
+
+# For Male, 40, College Graduate
+print(f"Predicted Probability of a male, 40 Years Old, College Graduate without Smoking Ban: {round(base_i[0], 3)}")
+print(f"Predicted Probability of a male, 40 Years Old, College Graduate with Smoking Ban: {round(ban_i[0], 3)}")
+print(f"Effect of Smoking Ban on Smoking Probability for Males: {round(base_i[0] - ban_i[0], 3)}")
+
+# Explanation:
+# Among 40-year-old males who are college graduates, the probability of smoking in the workplace
+# without any measures taken is 18.67%. However, if a smoking ban is implemented, this probability decreases to
+# 14.68%. Consequently, the smoking ban results in a reduction of smoking probability by 3.9% within this specific demographic group.
+
+
+# For Female, 20, High School Dropout
+print(f"Predicted Probability of a female, 20 Years Old, High School Dropout without Smoking Ban: {round(base_ii[0], 3)}")
+print(f"Predicted Probability of a female, 20 Years Old, High School Dropouts with Smoking Ban: {round(ban_ii[0], 3)}")
+print(f"Effect of Smoking Ban on Smoking Probability for Females: {round(base_ii[0] - ban_ii[0], 3)}")
+
+# Explanation:
+# For 20-year-old females who are high school dropouts, the lprobability of smoking in the workplace without any
+# measures taken is 36.14%. If a smoking ban is implemented, this probability decreases to 30.34%. Consequently,
+# the smoking ban results in a reduction of smoking probability by 5.8% within this specific demographic group.
+
+
+# Final Comparison
+base_i_df = probit_mod_fit.get_prediction(exog=[constant, smkban_i, age_i, age_sqrt_i, hsdrop_i, hsgrad_i, colsome_i, colgrad_i, female_i]).summary_frame()
+ban_i_df = probit_mod_fit.get_prediction(exog=[constant, 1, age_i, age_sqrt_i, hsdrop_i, hsgrad_i, colsome_i, colgrad_i, female_i]).summary_frame()
+i_df = pd.concat([base_i_df, ban_i_df]).reset_index(drop=True)
+i_df["Effect_strength"] = i_df.predicted.diff()
+print(i_df)
+
+base_ii_df = probit_mod_fit.get_prediction(exog=[constant, smkban_ii, age_ii, age_sqrt_ii, hsdrop_ii, hsgrad_ii, colsome_ii, colgrad_ii, female_ii]).summary_frame()
+ban_ii_df = probit_mod_fit.get_prediction(exog=[constant, 1, age_ii, age_sqrt_ii, hsdrop_ii, hsgrad_ii, colsome_ii, colgrad_ii, female_ii]).summary_frame()
+
+ii_df = pd.concat([base_ii_df, ban_ii_df]).reset_index(drop=True)
+ii_df["Effect_strength"] = ii_df.predicted.diff()
+print(ii_df)
